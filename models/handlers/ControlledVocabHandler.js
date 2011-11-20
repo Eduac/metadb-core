0425a9b29c29166617abcdadd5a8c19f029d6253
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2011-11-20 10:25:13 -0500

    add controlled vocab handler

---
+++ models/handlers/ControlledVocabHandler.js
@@ -0,0 +1,18 @@
+//Require db stuff here??
+var ControlledVocabHandler = (function () {
+	return {
+		name: 'ControlledVocabHandler', 
+		create: function (name, contents) {
+			//blah
+		},
+		remove: function (name, reason) {
+		 	//blah
+		},
+		update: function (name, contents) {
+			//blah
+		},
+		get: function (name) {
+			//blah
+		}
+	}
+})();
