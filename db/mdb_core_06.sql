1b6f30e8858b12270767e7f771170901fc941d33
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2011-12-04 18:22:09 -0500

    add sha-1 passwords

--- db/mdb_core_06.sql
+++ db/mdb_core_06.sql
@@ -1,3 +1,16 @@
 ALTER TABLE profiles
-ADD COLUMN password VARCHAR(40)
+ADD COLUMN password CHAR(40)
+;
+
+UPDATE profiles 
+SET password = 'bd3027fa569ea15ca76d84db21c67e2d514c1a5a'
+WHERE username = 'long'
+;
+
+UPDATE profiles 
+SET password ='a93eb5dc54e7bbc6ff4d3ad1cbd94968862357ac' 
+WHERE username = 'haruki'
+;
+
+UPDATE properties SET value = '6' WHERE name = 'version'
 ;
