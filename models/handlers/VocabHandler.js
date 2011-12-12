59cdc657fb214d4244986289ea0880499ecf606d
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2011-12-12 00:41:13 -0500

    Update models/handlers/VocabHandler.js

--- models/handlers/VocabHandler.js
+++ models/handlers/VocabHandler.js
@@ -1,15 +1,19 @@
 var VocabHandler = (function (daoList) {
     return {
         name : 'VocabHandler', 
+        
         create: function (vocabObj) {
                 vocabDAO.createVocab(vocabName, contents);
         },
+        
         get: function (id) {
             //blah
         },
+        
         update: function (vocabId, contents) {
             //blah
         },
+        
         remove: function (id, reason) {
             //blah
         }
