6ea1acc41b81bd4c4b220a8edb45b4e3a3479acd
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2011-12-12 00:38:59 -0500

    Update models/handlers/AuthenticationHandler.js

--- models/handlers/AuthenticationHandler.js
+++ models/handlers/AuthenticationHandler.js
@@ -1,27 +1,25 @@
 var uuid = require('node-uuid');
 var AuthenticationHandler = (function () {
-    var AuthDao = (require('../dao/AuthenticationDao')); 
-		var SessionDao = (require('../dao/SessionDao'));
-    var ProfileDao = (require('../dao/ProfileDao'));
-    
-    return {
-			name : 'AuthenticationHandler',
+	var AuthDao = (require('../dao/AuthenticationDao')); 
+	var SessionDao = (require('../dao/SessionDao'));
+	var ProfileDao = (require('../dao/ProfileDao'));
+	
+	return {
+		name : 'AuthenticationHandler',
 		
-			authenticate : function (username, password) {
-				
-				//Auth success -> return new session ID
-				//Auth failure -> return null
-				//TODO: Try-Catch 
-
-				if (authDao.authenticate(username, password)) {
-					var profile_id = profileDao.getProfile(username);
-					var uuid = sessionDao.createSession(profile_id);
-					return uuid;
-				}
-				else {
-					return null;
-				}
+		//Auth success -> return new session ID
+		//Auth failure -> return null
+		//TODO: Try-Catch 
+		authenticate : function (username, password) {
+			if (authDao.authenticate(username, password)) {
+				var profile_id = profileDao.getProfile(username);
+				var uuid = sessionDao.createSession(profile_id);
+				return uuid;
+			}
+			else {
+				return null;
 			}
 		}
+	}
 })();
 module.exports = AuthenticationHandler;
