5f873f57cde1cb3b9da04cc0d8ca0ef073ba31c1
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2011-12-12 00:36:37 -0500

    Update models/dao/VocabDao.js

--- models/dao/VocabDao.js
+++ models/dao/VocabDao.js
@@ -3,27 +3,22 @@ var pg = require('pg');
 var connectionString = "postgres://metadb_rw:metadb@localhost:5432/metadb";
 
 var VocabDao = (function () {
-    
 	return {
-		
 		name : 'VocabDao',
-        
+		
 		createVocab: function ( vocabName, contents ) {
 			pg.connect(connectionString, function (err, client) { 
-				client.query('INSERT INTO vocabs(vocab_id, name, words) '+
-						 	 ' VALUES($1, $2, $3) ',
-							 [ uuid(), vocabName, contents ]
-                        	);
-            });
-        },
-
-        removeVocab: function ( vocabId ) {
-            pg.connect(connectionString, function (err, client) {
-                client.query('DELETE FROM vocabs WHERE vocab_id = ?', 
-                              [ vocabId ]
-                            );
-            });
-        }
-    }
+				client.query('INSERT INTO vocabs(vocab_id, name, words) VALUES($1, $2, $3)', 
+						[ uuid(), vocabName, contents ]);
+			});
+		},
+		
+		removeVocab: function ( vocabId ) {
+			pg.connect(connectionString, function (err, client) {
+				client.query('DELETE FROM vocabs WHERE vocab_id = ?',
+						[ vocabId ]);
+			});
+		}
+	}
 })();
 module.exports = VocabDao;
