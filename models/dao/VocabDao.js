841c5e0cc9474782dbd3e35ce46e49f051ecb39b
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2011-12-24 04:41:55 -0500

    Complete CRUD in VocabDao, add vows tests for VocabDao

--- models/dao/VocabDao.js
+++ models/dao/VocabDao.js
@@ -1,11 +1,16 @@
 var uuid = require('node-uuid')
 , 	CrudDao = require('./CrudDao')
 , 	_queries = {
-		create : 'INSERT INTO vocabs(vocab_id, name, words) VALUES($1, $2, $3)',
-		deleteById : 'DELETE FROM vocabs WHERE vocab_id = $1'
+		create : 'INSERT INTO controlled_vocabs(vocab_id, name, words) VALUES($1, $2, $3)',
+		findById : 'SELECT * FROM controlled_vocabs WHERE vocab_id = $1',
+		update : 'UPDATE controlled_vocabs SET name = $2, words = $3 WHERE vocab_id = $1',	
+		deleteById : 'DELETE FROM controlled_vocabs WHERE vocab_id = $1',
+		findByVocabName : 'SELECT * FROM controlled_vocabs WHERE name = $1'
 };
 
 var VocabDao = CrudDao.extend({
+	
+	/******* Override *******/ 
 	queries : _queries,
 	objToQuery : function (obj) {
 		return [
@@ -20,6 +25,24 @@ var VocabDao = CrudDao.extend({
 			name : row.name,
 			words : row.words
 		};
+	},
+
+	create : function (obj, callbackFn) {
+		obj.vocab_id = obj.id = uuid();
+		this._super (obj, callbackFn);
+	},
+
+	/******* Custom *******/
+	findByVocabName: function (vocabName, callbackFn) {
+		if (typeof callbackFn !== 'function') return;
+		var _this = this; 
+		this.connect(function (err, client) {
+			var object; 
+			client
+				.query(_this.queries.findByVocabName, [vocabName])
+				.on('row', function (row) { object = _this.queryToObj(row); })
+				.on('end', function () { callbackFn(object); });
+				});
 	}
 });
 module.exports = new VocabDao();
---
+++ test/daos/VocabDaoTest.js
@@ -0,0 +1,55 @@
+var assert = require('assert')
+,   vows = require('vows')
+,   events = require('events')
+,   vocabDao = require('../../models/dao/VocabDao')
+,	getTestVocab = function () {
+		return {
+			name : 'test_vocab',
+			words: 'one; two; three; four'
+		};	
+	};
+	
+vows.describe('VocabDao').addBatch({
+    'after initialization' : {
+        'should have findById query' : function () {
+            assert.equal(vocabDao.queries.findById, 'SELECT * FROM controlled_vocabs WHERE vocab_id = $1');            
+        },
+        'should inherit findById method' : function () {
+            assert.ok(vocabDao.findById);            
+        },
+        'after creating a sample vocabulary' : {
+            topic : function () {
+                var promise = new events.EventEmitter();
+                vocabDao.create(getTestVocab(), function (vocab) {
+                    promise.emit('success', vocab);
+                });
+                return promise;
+            },
+            'should return the vocab with a populated id' : function (vocab) {
+                assert.ok(vocab.vocab_id);
+            },
+            'should be able to find that vocab from the DB' : {
+                topic : function (vocab) {
+                    var promise = new events.EventEmitter();
+                    vocabDao.findById(vocab.vocab_id, function (vocab) {
+                        promise.emit('success', vocab);
+                    });
+                    return promise;
+                },
+                'and that vocab should exist' : function (vocab) { 
+                    assert.ok(vocab); 
+                },
+                'and that vocab should have same name' : function (vocab) { 
+                    assert.equal(vocab.name, 'test_vocab'); 
+                },
+                'and that vocab should have the same contents' : function (vocab) { 
+                    assert.equal(vocab.words, 'one; two; three; four'); 
+                }
+            },
+            teardown : function (vocab) {
+                var callback = this.callback;
+                setTimeout(function () { vocabDao.deleteById(vocab.vocab_id, callback); }, 100);
+            }
+        }
+    }
+}).export(module);
