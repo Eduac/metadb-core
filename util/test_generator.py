89555962a210a3e694de53fb842ffaa805759d62
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2012-01-25 23:31:55 -0500

    Script update

--- util/test_generator.py
+++ util/test_generator.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
 '''
+====
 MetaDB 4 Test Data Generator Script
 Haruki Yamaguchi, Long Ho 
 February 2012
 
-Input: Minimal. Takes target location of dummy master files 
-Output: 1x .SQL file containing queries to create the test harness 
-        1x .SQL file to tear down the test harness
-'''
+Generates a .sql file with complete test data for MetaDB.
 
-'''
-Python modules/functionality needed
+TODO: Add the masterfile/deriv dummy generation. This file currently only generates data for the database to import.
 
---random word generator (for random vocabs)
---random color generator (for dummy images)
---uuid generator
+Input: None
+Output: 1x .SQL file to tear down and setup the test harness
 
---
+====
+Steps for test data generation
 
-'''
+1. Generate dummy IDs for permissions 
+2. Generate dummy controlled vocabs
+3. Generate test metadata schema
+4. Generate features (admin/desc/tech) <--how to make system parse tech?
+5. Generate projects
+6. Generate project metadata
+7. Generate project features
+8. Generate test elements
+9. Generate items
+10. Generate permissions
+11. Generate item data
 
-
-'''
+=====
 Dependency List (order in which tables must be populated with test data)
+Any order will work within the dependency level.
 
-NO DEPENDENCIES
+NO DEPENDENCIES (Data in these tables will be explicitly torn down)
 profiles (profile_id, username, email, first_name, last_name, last_login, last_project_id, role, password) 
 controlled_vocabs (vocab_id, name, words)
 metadata_schemas (schema_id, name, description) 
@@ -35,12 +42,14 @@ properties (name, value)
 
 ONE DEPENDENCY
 elements (element_id, schema_id, element)
-item_data (item_id, data)
 project_metadata (project_id, derivative, width, height, brand, background_color, foreground_color)
 
 TWO DEPENDENCIES
 items (item_id, project_id, item_index, element_id, value)
 permissions (profile_id, project_id, feature_bit_mask)
+
+THREE DEPENDENCIES
+item_data (item_id, data) <indirectly depends on project/element>
 
 FOUR DEPENDENCIES
 project_features (project_id, feature_id, element_id, value, setting_bit_mask, vocab_id)
@@ -49,12 +58,12 @@ project_features (project_id, feature_id, element_id, value, setting_bit_mask, v
 import csv
 import random
 import uuid
-import sha
+import hashlib 
 import re
+import string
 
-PROJECT_COUNT=5
-FEATURE_COUNT=3
-ITEM_COUNT=100
+def gen_delete_query (table, idField, idVal):
+	return "DELETE FROM "+table+" WHERE "+idField+'= \''+idVal+'\';'
 
 '''
 Generates a insert query for table. Assume var and vals match up together (ignore the ID column)
@@ -67,98 +76,96 @@ def gen_insert_query (table, ids, columns, vals_format, vals):
   query+= ") "
   query+= "VALUES (" 
   query+= ', '.join(map(str, vals_format)) % tuple(vals) 
-  query+= ")" 
+  query+= ");" 
 
   result['query'] = query
   return result
-
-'''
-By table
+''' By table
 '''
 def gen_create_profile (user):
   table = 'profiles'
   ids = {}
-  ids['profile_id'] = uuid.uuid4().hex
+  ids['profile_id'] = user ['profile_id'] 
   columns = ['profile_id', 'username', 'email', 'first_name', 'last_name', 'last_login', 'last_project_id', 'role', 'password'] 
   vals_format = ['\'%s\'', '\'%s\'',  '\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'']
-  vals = [ids['profile_id'], user['username'], user['email'], user['first_name'], user['last_name'], user['last_login'], user['last_project_id'], user['role'], user['password']]
+  vals = [user['profile_id'], user['username'], user['email'], user['first_name'], user['last_name'], user['last_login'], user['last_project_id'], user['role'], user['password']]
   result = gen_insert_query (table, ids, columns, vals_format, vals)
   return result
 
 def gen_create_vocab (vocab):
   table = 'controlled_vocabs'
   ids = {}
-  ids['vocab_id'] = uuid.uuid4().hex
+  ids['vocab_id'] = vocab['vocab_id'] 
   columns = ['vocab_id', 'name', 'words']
   vals_format = ['\'%s\'', '\'%s\'', '\'%s\'']
-  vals = [ids['vocab_id'], vocab['name'], vocab['words']]
+  vals = [vocab['vocab_id'], vocab['name'], vocab['words']]
   result = gen_insert_query (table, ids, columns, vals_format, vals)
   return result
 
 def gen_create_schema (schema):
   table = 'metadata_schemas'
   ids = {}
-  ids['schema_id'] = uuid.uuid4().hex
+  ids['schema_id'] = schema['schema_id'] 
   columns = ['schema_id', 'name', 'description']
   vals_format = ['\'%s\'', '\'%s\'', '\'%s\'']
-  vals = [ids['schema_id'], schema['name'], schema['description']]
+  vals = [schema['schema_id'], schema['name'], schema['description']]
   result = gen_insert_query (table, ids, columns, vals_format, vals)
   return result
 
 def gen_create_feature (feature): 
   table = 'features'
   ids = {}
-  ids['feature_id'] = uuid.uuid4().hex
+  ids['feature_id'] = feature['feature_id'] 
   columns = ['feature_id', 'name', 'description', 'feature_bit']
   vals_format = ['\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'']
-  vals = [ids['feature_id'], feature['name'], feature['description'], feature['feature_bit']]
+  vals = [feature['feature_id'], feature['name'], feature['description'], feature['feature_bit']]
   result = gen_insert_query (table, ids, columns, vals_format, vals)
   return result
 
 def gen_create_setting (setting):
   table = 'settings'
   ids = {}
-  ids['setting_id'] = uuid.uuid4().hex
+  ids['setting_id'] = setting['setting_id'] 
   columns = ['setting_id', 'name' 'bit']
   vals_format = ['\'%s\'', '\'%s\'', '\'%s\'']
-  vals = [ids['setting_id'], setting['name'], setting['bit']]
+  vals = [setting['setting_id'], setting['name'], setting['bit']]
   result = gen_insert_query (table, ids, columns, vals_format, vals)
   return result
 
 def gen_create_element (element):
-  table = 'elements'
-  ids = {}
-  ids['element_id'] = uuid.uuid4().hex
-  ids['schema_id'] = element['schema_id']
-  columns = ['element_id', 'schema_id', 'element']
-  vals_format = ['\'%s\'', '\'%s\'', '\'%s\'']
-  vals = [ids['element_id'], setting['name'], setting['bit']]
-  result = gen_insert_query (table, ids, columns, vals_format, vals)
-  return result
+	table = 'elements'
+	ids = {}
+	ids['schema_id'] = element['schema_id']
+	ids['element_id'] = element['element_id']
+	columns = ['element_id', 'schema_id', 'element']
+	vals_format = ['\'%s\'', '\'%s\'', '\'%s\'']
+	vals = [element['element_id'], element['schema_id'], element['element']]
+	result = gen_insert_query (table, ids, columns, vals_format, vals)
+	return result
 
-def gen_create_project ( name, description, branding_text ):
-  table = 'projects'
-  ids = {}
-  ids['project_id'] = uuid.uuid4().hex
-  columns = ['project_id', 'name', 'description', 'branding_text']
-  vals_format = ['\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'']
-  vals = [ids['element_id'], project['name'], project['description'], project['branding_text']]
-  result = gen_insert_query (table, ids, columns, vals_format, vals)
-  return result
+def gen_create_project (project):
+	table = 'projects'
+	ids = {}
+	ids['project_id'] = project['project_id'] 
+	columns = ['project_id', 'name', 'description', 'branding_text']
+	vals_format = ['\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'']
+	vals = [project['project_id'], project['name'], project['description'], project['branding_text']]
+	result = gen_insert_query (table, ids, columns, vals_format, vals)
+	return result
 
-def gen_create_property (property):
-  table = 'properties'
-  ids = {}
-  columns = ['name', 'value']
-  vals_format = ['\'%s\'', '\'%s\'']
-  vals = [property['name'], property['value']]
-  result = gen_insert_query (table, ids, columns, vals_format, vals)
-  return result
+def gen_create_property (prop):
+	table = 'properties'
+	ids = {}
+	columns = ['name', 'value']
+	vals_format = ['\'%s\'', '\'%s\'']
+	vals = [prop['name'], prop['value']]
+	result = gen_insert_query (table, ids, columns, vals_format, vals)
+	return result
 
 def gen_create_item_data ( item_data):
   table = 'item_data'
   ids = {}
-  ids['item_id'] = item_data[item_id]
+  ids['item_id'] = item_data['item_id']
   columns = ['item_id', 'data']
   vals_format = ['\'%s\'', '\'%s\'']
   vals = [item_data['item_id'], item_data['data']]
@@ -178,7 +185,7 @@ def gen_create_project_metadata (project_md):
 def gen_create_item (item):
   table = 'items'
   ids = {}
-  ids['item_id'] = uuid.uuid4().hex
+  ids['item_id'] = item['item_id'] 
   ids['project_id'] = item['project_id']
   columns = ['item_id', 'project_id', 'item_index', 'element_id', 'value']
   vals_format = ['\'%s\'', '\'%s\'', '\'%s\'', '\'%s\'', '\'%s\''] 
@@ -210,44 +217,244 @@ def gen_create_project_feature (project_feature):
   result = gen_insert_query (table, ids, columns, vals_format, vals)
   return result
 
+def lorem_ipsum (amount):
+	text = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer
+	eu lacus accumsan arcu fermentum euismod. Donec pulvinar porttitor
+	tellus. Aliquam venenatis. Donec facilisis pharetra tortor.  In nec
+	mauris eget magna consequat convallis. Nam sed sem vitae odio
+	pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
+	metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
+	urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
+	suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
+	adipiscing. Suspendisse eu lectus. In nunc. Duis vulputate tristique
+	enim. Donec quis lectus a justo imperdiet tempus."""
+	words= text.split()
+	return " ".join(words[0:max(len(words), amount)])
 
-'''
-Steps for test data generation
+profiles = []
+userNames=  [ 'lacus', 'kira', 'athrun', 'cagalli', 'mu']
+firstNames = [ 'Lacus', 'Kira', 'Athrun', 'Cagalli', 'Mu' ]
+email = 'dummy@dummy.com'
+lastNames = [ 'Clyne', 'Yamato', 'Zala', 'Yula Athha', 'La Flaga']
+password = '123123'
 
-1. Generate dummy IDs for permissions 
-2. Generate dummy controlled vocabs
-3. Generate test metadata schema
-4. Generate features (admin/desc/tech) <--how to make system parse tech?
-5. Generate projects
-6. Generate project metadata
-7. Generate project features
-8. Generate test elements
-9. Generate items
-10. Generate permissions
-11. Generate item data
-'''
-doManual = raw_input("Generate automatically? (y/n)")
-while doManual != "y" and doManual != "n":
-  doManual = raw_input("Generate a random test script? Enter yes or no.")
+numProfiles = len(userNames)
 
-if doManual:
-  numProfiles = raw_input ("Enter # of users")
-  for i in range(numProfiles):
-    profile = {}
-    profile['profile_id'] = uuid.uuid4().hex
+for i in range(numProfiles):
+	profile = {}
+	profile['profile_id'] = uuid.uuid4().hex
+	profile['username'] = userNames[i]
+	profile['email'] = email
+	profile['first_name'] = firstNames[i]
+	profile['last_name'] = lastNames[i]
+	profile['last_login'] = 0
+	profile['last_project_id'] = ''
+	profile['role'] = 'worker'
+	sha = hashlib.sha1()
+	sha.update('789789')
+	profile['password'] = sha.hexdigest()
+	profiles.append(profile)
 
-    username = raw_input("Enter username")
-    rx = re.compile('\W') 
-    while rx.match(username).group != None and len(username)> 20 and username == "":
-      username = raw_input("Enter username Only alphanumeric and underscores; under 20 characters")
-    profile['username'] = username
 
-    password = raw_input("Enter user's password")
-    while password == "":
-      password = raw_input("Enter a non-blank password")
-    profile['password'] = sha1(password)
-    
-  numVocabs = raw_input ("Enter # of vocabs")
-  numProjects = raw_input("Enter # of projects")
-  numItems = raw_input("Enter # of items per proj")
-  masterPath = raw_input("Enter the path to dummy master files (these must later be removed MANUALLY)")
+### 
+# Generate Vocabs
+###
+numVocabs = 5
+vocabs = []
+for i in range(numVocabs):
+	vocabs.append( { 'vocab_id' : uuid.uuid4().hex, 
+									 'name' :'vocab_'+str(i),
+									 'words' : ", ".join(list("".join(list([random.choice(string.letters) for i in xrange(8)])) for j in xrange(10))) })
+
+###
+# Generate projects
+###
+numProjects = 5
+projects=[]
+for i in range(numProjects):
+	projects.append( 
+		{ 
+			'project_id' : uuid.uuid4().hex, 
+			'name' : 'project_'+str(i),
+			'description' : 'Test description for project_'+str(i),
+			'branding_text' : 'Test brand for project_'+str(i)
+		})
+
+metadata_schemas = []
+metadata_schemas.append(
+		{ 
+			'schema_id' : uuid.uuid4().hex,
+			'name' : 'test_schema',
+			'description' : 'Test schema.'
+		})
+
+features = []
+features.append(
+		{ 
+			'feature_id' : uuid.uuid4().hex,
+			'name' : 'descriptive',
+			'description' : 'Descriptive metadta',
+			'feature_bit' : '0'
+		})
+
+features.append(
+		{ 
+			'feature_id' : uuid.uuid4().hex,
+			'name' : 'administrative',
+			'description' : 'Administrative metadta',
+			'feature_bit' : '00'
+		})
+
+elements = []
+for schema in metadata_schemas:
+	for i in range(10):
+		elements.append(
+				{
+					'element_id' : uuid.uuid4().hex,
+					'schema_id' : schema['schema_id'],
+					'element' : schema['name']+'_element_'+str(i)
+				})
+
+items = []
+for project in projects:
+		for i in range(100):
+			for element in elements:
+				items.append(
+					{
+						'item_id' : uuid.uuid4().hex,
+						'project_id' : project['project_id'],
+						'item_index' : i,
+						'element_id' : element['element_id'],
+						'value' : 'value'
+					})
+
+item_data = []
+for item in items:
+	item_data.append(
+			{
+				'item_id' : item['item_id'],
+				'data' : lorem_ipsum(random.randint(1, 20))
+			})
+
+permissions = []
+for profile in profiles:
+	for project in projects:
+		permissions.append(
+				{
+					'profile_id' : profile['profile_id'],
+					'project_id' : project['project_id'],
+					'feature_bit_mask' : '00'
+				})
+
+project_metadata = []
+for project in projects: 
+	project_metadata.append(
+			{
+				'project_id' : project['project_id'],
+				'derivative' : 'thumb',
+				'width' : 300,
+				'height' : 300,
+				'brand' : 'Test brand',
+				'background_color' : '00000000',
+				'foreground_color' : 'ffffffff'
+			})
+	
+	project_metadata.append(
+			{
+				'project_id' : project['project_id'],
+				'derivative' : 'zoom',
+				'width' : 2000,
+				'height' : 2000,
+				'brand' : 'Test brand',
+				'background_color' : '00000000',
+				'foreground_color' : 'ffffffff'
+			})
+	
+	project_metadata.append(
+			{
+				'project_id' : project['project_id'],
+				'derivative' : 'full',
+				'width' : 0,
+				'height' : 0,
+				'brand' : 'Test brand',
+				'background_color' : '00000000',
+				'foreground_color' : 'ffffffff'
+			})
+
+###
+# Bad code here. element_id is fine but value is always hardcoded
+project_features = []
+for project in projects:
+	for feature in features:
+		for element in elements:
+			project_features.append(
+					{
+						'project_id' : project['project_id'],
+						'feature_id' : feature['feature_id'],
+						'element_id' : element['element_id'],
+						'value' : 'value',
+						'setting_bit_mask' : 0,
+						'vocab_id' : ''
+					})
+
+createCommands=[]
+
+for profile in profiles:
+	createCommands.append(gen_create_profile(profile))
+
+for vocab in vocabs:
+	createCommands.append(gen_create_vocab(vocab))
+
+for schema in metadata_schemas:
+	createCommands.append(gen_create_schema(schema))
+
+for feature in features:
+	createCommands.append(gen_create_feature(feature))
+
+for project in projects:
+	createCommands.append(gen_create_project(project))
+
+for element in elements:
+	createCommands.append(gen_create_element(element))
+
+for metadata in project_metadata:
+	createCommands.append(gen_create_project_metadata(metadata))
+
+for item in items:
+	createCommands.append(gen_create_item(item))
+
+for permission in permissions:
+	createCommands.append(gen_create_permission(permission))
+
+for data in item_data:
+	createCommands.append(gen_create_item_data(data))
+
+for pj_feature in project_features:
+	createCommands.append(gen_create_project_feature(pj_feature))
+
+removeCommands = []
+for profile in profiles:
+	removeCommands.append(gen_delete_query('profiles', 'profile_id', profile['profile_id']))
+
+for vocab in vocabs:
+	removeCommands.append(gen_delete_query('controlled_vocabs', 'vocab_id', vocab['vocab_id']))
+
+for schema in metadata_schemas:
+	removeCommands.append(gen_delete_query('metadata_schemas', 'schema_id', schema['schema_id']))
+
+for feature in features:
+	removeCommands.append(gen_delete_query('features', 'feature_id', feature['feature_id']))
+
+for project in projects: 
+	removeCommands.append(gen_delete_query('projects', 'project_id', project['project_id']))
+
+outputFile = open("test_data.sql", "w")
+for command in removeCommands:
+	outputFile.write(command+'\n')
+
+for i in range(3):
+	outputFile.write('\n')
+
+for command in createCommands:
+	outputFile.write(command['query']+'\n')
+outputFile.close()
